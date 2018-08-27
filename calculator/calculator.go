c7c87ce60ea0312f5076a187b0bde7f19ef77b6d
zaccoding <zaccoding725@gmail.com>
2018-08-27 23:04:03 +0900

    chap 02

---
+++ calculator/calculator.go
@@ -0,0 +1,9 @@
+package calculator
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Subtract(a int, b int) int {
+	return a - b
+}
---
+++ feb/fib.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zacscoding/gogo/seq"
+)
+
+func main() {
+	fmt.Println(seq.Fib(6))
+}
